38422b976644bb655ebcdc6d23c3fb56e267ee64
alex Jarschel <alexbaukartjarschel@gmail.com>
2023-03-22 14:46:56 -0300

    consumindo API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log("teste");
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+const list = fetch(url)
+.then((response) => response.json())
+.then((jsonBody) => jsonBody.results)
+.then((pokemonList) =>{
+ console.log(pokemonList[0])})
+
+.catch((error) => console.error(error))
+
+console.log(list)
+
