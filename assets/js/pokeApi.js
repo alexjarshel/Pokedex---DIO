abbe214555ac5f4a3374e8611638410c317e1d41
unknown <alexbaukartjarschel@gmail.com>
2023-05-06 15:21:12 -0300

    adicionando cores

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -2,7 +2,7 @@ const pokeApi ={}
 
 
 
-pokeApi.getPokemons = (offset = 0, limit  = 151) =>{
+pokeApi.getPokemons = (offset = 0, limit  = 150) =>{
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     
@@ -15,6 +15,23 @@ pokeApi.getPokemons = (offset = 0, limit  = 151) =>{
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon;
+}
